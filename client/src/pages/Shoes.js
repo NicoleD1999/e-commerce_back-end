0f1f66f19686c4d0667f86955117c7d45ef63c06
NicoleD1999 <nmd1018@yahoo.com>
2022-09-02 14:53:04 -0500

    began implementing axios to fetch routes

--- client/src/pages/Shoes.js
+++ client/src/pages/Shoes.js
@@ -1,9 +1,31 @@
-import { Link } from "react-router-dom";
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios'
+// import {useHistory} from 'react-router-dom'
+import '../App.css'
 
 function Shoes(){
+    const [shoeList, setShoeList]= useState([])
+    // let history = useHistory()
+
+    useEffect(()=>{
+        Axios.get('http://localhost:3001/api/products').then((data)=>{
+            setShoeList(data)
+            console.log(data)
+        })
+    },[])
+    
     return (
-        
+        <div>
+            {shoeList.map((val, key)=>{
+                return (
+                    <div>
+                        <h1>
+                            {val}
+                        </h1>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
-
 export default Shoes
